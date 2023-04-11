dbba0d6c4493c96350135cbb514ece4ec443d443
Szymon Wałach <72652252+szymonisokay@users.noreply.github.com>
2023-04-11 21:27:30 +0200

    create error middleware

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express'
+import { ResponseModel } from '../shared/interfaces/response.interface'
+
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+	const statusCode: number = res.statusCode ? res.statusCode : 500
+
+	res.status(statusCode)
+	res.json({ result: null, message: err.message } as ResponseModel)
+}
